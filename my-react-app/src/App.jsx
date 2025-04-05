a8d774659ff8f242144c4dd4ff5c5086629a03c7
sprevaux2026 <stevenprevaux@gmail.com>
2025-04-05 11:42:07 -0400

    Use the component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Import Greeting component
+import Greeting from './components/Greeting'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,18 +19,22 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Edit <code>src/App.jsx</code> and save to test HMR</p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      {/* Use the Greeting component below */}
+      <Greeting />
     </>
   )
 }
