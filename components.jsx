ccc7f41435f3d7ddbf7342dff3852d8a083dd1c9
sprevaux2026 <stevenprevaux@gmail.com>
2025-04-05 11:35:52 -0400

    Setup React app and greeting.jsx

---
+++ components.jsx
@@ -0,0 +1,13 @@
+// Greeting.jsx
+import React from 'react';
+//Make greeting component
+function Greeting() {
+  return (
+    <div>
+      <h2>Hello from the Greeting Component!</h2>
+      <p>This is your first custom component in React.</p>
+    </div>
+  );
+}
+
+export default Greeting;
