f103df9ad534b110912fd5e27c4b549319bf223c
sprevaux2026 <stevenprevaux@gmail.com>
2025-04-05 11:59:35 -0400

    Fixed formatting issues

---
+++ my-react-app/src/Components/Greeting.jsx
@@ -0,0 +1,13 @@
+// Greeting.jsx
+import React from 'react';
+
+function Greeting() {
+  return (
+    <div>
+      <h2>Hello from the Greeting Component!</h2>
+      <p>This is my first custom component in React.</p>
+    </div>
+  );
+}
+
+export default Greeting;
